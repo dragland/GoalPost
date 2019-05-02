81fafa32a23ef77ca380bc439738d805e9ea0cd3
Cherry <chzou2009@gmail.com>
2019-05-02 16:37:49 -0700

    prettier

--- GoalPost/src/screens/createGoal.js
+++ GoalPost/src/screens/createGoal.js
@@ -35,13 +35,8 @@ class CreateGoal extends React.Component {
 
 
 render() {
- return (
-  <View style={{ 
-   flex: 1,
-   alignItems:'center',
-   justifyContent:'flex-start'
-  }}>
-
+  return (
+  <View style={styles.screen}>
    <View style={styles.inputRow}>
      <View style={styles.inputHeaderContainer}>
        <Text style={styles.inputHeader}>Goal Name</Text>
@@ -55,37 +50,57 @@ render() {
    </View>
 
    <View style={styles.inputRow}>
-     <Text style={styles.inputHeader}>Start Date</Text>
-     <Input
-       value={this.state.date}
-       onFocus={this.showDateTimePicker}
-     />
-     <DateTimePicker
-       isVisible={this.state.isDateTimePickerVisible}
-       onConfirm={this.handleDatePicked}
-       onCancel={this.hideDateTimePicker}
-     />
+     <View style={styles.inputHeaderContainer}>
+       <Text style={styles.inputHeader}>Start Date</Text>
+     </View>
+     <View style={styles.inputTakerContainer}>
+       <Input
+         value={this.state.date}
+         onFocus={this.showDateTimePicker}
+       />
+       <DateTimePicker
+         isVisible={this.state.isDateTimePickerVisible}
+         onConfirm={this.handleDatePicked}
+         onCancel={this.hideDateTimePicker}
+       />
+     </View>
    </View>
 
    <View style={styles.inputRow}>
-     <Text style={styles.inputHeader}>Repeat?</Text>
-     <Input placeholder='Weekly M, W, F' />
+     <View style={styles.inputHeaderContainer}>
+       <Text style={styles.inputHeader}>Repeat?</Text>
+     </View>
+     <View style={styles.inputTakerContainer}>
+       <Input placeholder='Weekly M, W, F' />
+     </View>
    </View>
    <View style={styles.inputRow}>
-     <Text style={styles.inputHeader}>Cost/miss</Text>
-     <Input placeholder='$5.00' />
+     <View style={styles.inputHeaderContainer}>
+       <Text style={styles.inputHeader}>Cost/miss</Text>
+     </View>
+     <View style={styles.inputTakerContainer}>
+       <Input placeholder='$5.00' />
+     </View>
    </View>
    <View style={styles.inputRow}>
-     <Text style={styles.inputHeader}>Members</Text>
-     <Input placeholder='Choose from FB' />
+     <View style={styles.inputHeaderContainer}>
+       <Text style={styles.inputHeader}>Members</Text>
+     </View>
+     <View style={styles.inputTakerContainer}>
+       <Input placeholder='Choose from FB' />
+     </View>
    </View>
 
    <View style={styles.inputRow}>
-     <Text style={styles.inputHeader}>Push notifications?</Text>
-     <Switch
-       onValueChange={this.togglePushNotifs}
-       value={this.state.enablePushNotifs}
-     />
+     <View style={styles.inputHeaderContainer}>
+       <Text style={styles.inputHeader}>Push notifications?</Text>
+     </View>
+     <View style={styles.inputTakerContainer}>
+       <Switch
+         onValueChange={this.togglePushNotifs}
+         value={this.state.enablePushNotifs}
+       />
+     </View>
    </View>
 
    <Button title="Go to Goal Completion screen"
@@ -103,10 +118,16 @@ render() {
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'flex-start',
+  },
   inputRow: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
+    padding: 20,
   },
   inputHeaderContainer: {
     flex: 0.4,
@@ -118,7 +139,8 @@ const styles = StyleSheet.create({
     lineHeight: 21,
   },
   inputTakerContainer: {
-    flex: 0.5,
+    flex: 0.6,
+    alignItems: 'flex-start',
   },
 });
 
