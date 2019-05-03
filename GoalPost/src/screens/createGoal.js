ddff1434eddd7f937e85c5d18f5f2a21a3ebe5b0
Cherry <chzou2009@gmail.com>
2019-05-02 17:27:56 -0700

    prettier

--- GoalPost/src/screens/createGoal.js
+++ GoalPost/src/screens/createGoal.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Button, View, StyleSheet, Switch, Text } from 'react-native';
-import { Input } from 'react-native-elements';
-import DateTimePicker from 'react-native-modal-datetime-picker';
+import React from "react";
+import { Button, View, StyleSheet, Switch, Text } from "react-native";
+import { Input } from "react-native-elements";
+import DateTimePicker from "react-native-modal-datetime-picker";
 
 class CreateGoal extends React.Component {
   static navigationOptions = {
-    title: 'CreateGoal'
-   };
+    title: "CreateGoal"
+  };
   state = {
     isDateTimePickerVisible: false,
     enablePushNotifs: true,
     goalName: null,
-    date: null,
+    date: null
   };
 
-  updateGoalName = (e) => {
+  updateGoalName = e => {
     this.setState({ goalName: e.nativeEvent.text });
   };
 
@@ -33,115 +33,111 @@ class CreateGoal extends React.Component {
     this.setState({ enablePushNotifs: bool });
   };
 
+  render() {
+    return (
+      <View style={styles.screen}>
+        <View style={styles.inputRow}>
+          <View style={styles.inputHeaderContainer}>
+            <Text style={styles.inputHeader}>Goal Name</Text>
+          </View>
+          <View style={styles.inputTakerContainer}>
+            <Input placeholder="Gym Buddies" onChange={this.updateGoalName} />
+          </View>
+        </View>
 
-render() {
-  return (
-  <View style={styles.screen}>
-   <View style={styles.inputRow}>
-     <View style={styles.inputHeaderContainer}>
-       <Text style={styles.inputHeader}>Goal Name</Text>
-     </View>
-     <View style={styles.inputTakerContainer}>
-       <Input
-         placeholder='Gym Buddies' 
-         onChange={this.updateGoalName}
-       />
-     </View>
-   </View>
+        <View style={styles.inputRow}>
+          <View style={styles.inputHeaderContainer}>
+            <Text style={styles.inputHeader}>Start Date</Text>
+          </View>
+          <View style={styles.inputTakerContainer}>
+            <Input value={this.state.date} onFocus={this.showDateTimePicker} />
+            <DateTimePicker
+              isVisible={this.state.isDateTimePickerVisible}
+              onConfirm={this.handleDatePicked}
+              onCancel={this.hideDateTimePicker}
+            />
+          </View>
+        </View>
 
-   <View style={styles.inputRow}>
-     <View style={styles.inputHeaderContainer}>
-       <Text style={styles.inputHeader}>Start Date</Text>
-     </View>
-     <View style={styles.inputTakerContainer}>
-       <Input
-         value={this.state.date}
-         onFocus={this.showDateTimePicker}
-       />
-       <DateTimePicker
-         isVisible={this.state.isDateTimePickerVisible}
-         onConfirm={this.handleDatePicked}
-         onCancel={this.hideDateTimePicker}
-       />
-     </View>
-   </View>
+        <View style={styles.inputRow}>
+          <View style={styles.inputHeaderContainer}>
+            <Text style={styles.inputHeader}>Repeat?</Text>
+          </View>
+          <View style={styles.inputTakerContainer}>
+            <Input placeholder="Weekly M, W, F" />
+          </View>
+        </View>
+        <View style={styles.inputRow}>
+          <View style={styles.inputHeaderContainer}>
+            <Text style={styles.inputHeader}>Cost/miss</Text>
+          </View>
+          <View style={styles.inputTakerContainer}>
+            <Input placeholder="$5.00" />
+          </View>
+        </View>
+        <View style={styles.inputRow}>
+          <View style={styles.inputHeaderContainer}>
+            <Text style={styles.inputHeader}>Members</Text>
+          </View>
+          <View style={styles.inputTakerContainer}>
+            <Input placeholder="Choose from FB" />
+          </View>
+        </View>
 
-   <View style={styles.inputRow}>
-     <View style={styles.inputHeaderContainer}>
-       <Text style={styles.inputHeader}>Repeat?</Text>
-     </View>
-     <View style={styles.inputTakerContainer}>
-       <Input placeholder='Weekly M, W, F' />
-     </View>
-   </View>
-   <View style={styles.inputRow}>
-     <View style={styles.inputHeaderContainer}>
-       <Text style={styles.inputHeader}>Cost/miss</Text>
-     </View>
-     <View style={styles.inputTakerContainer}>
-       <Input placeholder='$5.00' />
-     </View>
-   </View>
-   <View style={styles.inputRow}>
-     <View style={styles.inputHeaderContainer}>
-       <Text style={styles.inputHeader}>Members</Text>
-     </View>
-     <View style={styles.inputTakerContainer}>
-       <Input placeholder='Choose from FB' />
-     </View>
-   </View>
+        <View style={styles.inputRow}>
+          <View style={styles.inputHeaderContainer}>
+            <Text style={styles.inputHeader}>Push notifications?</Text>
+          </View>
+          <View style={styles.inputTakerContainer}>
+            <Switch
+              onValueChange={this.togglePushNotifs}
+              value={this.state.enablePushNotifs}
+            />
+          </View>
+        </View>
 
-   <View style={styles.inputRow}>
-     <View style={styles.inputHeaderContainer}>
-       <Text style={styles.inputHeader}>Push notifications?</Text>
-     </View>
-     <View style={styles.inputTakerContainer}>
-       <Switch
-         onValueChange={this.togglePushNotifs}
-         value={this.state.enablePushNotifs}
-       />
-     </View>
-   </View>
-
-   <Button title="Go to Goal Completion screen"
-    onPress={() => this.props.navigation.navigate('Completion', {
-      goalName: this.state.goalName,
-    })}
-   />
-   <Button title="Go Back"
-    onPress={() => this.props.navigation.goBack()}
-   />
-
-  </View>
-);
-}
+        <Button
+          title="Go to Goal Completion screen"
+          onPress={() =>
+            this.props.navigation.navigate("Completion", {
+              goalName: this.state.goalName
+            })
+          }
+        />
+        <Button
+          title="Go Back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems:'center',
-    justifyContent:'flex-start',
+    alignItems: "center",
+    justifyContent: "flex-start"
   },
   inputRow: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 20
   },
   inputHeaderContainer: {
-    flex: 0.4,
+    flex: 0.4
   },
   inputHeader: {
-    color: '#484848',
-    fontWeight: 'bold',
+    color: "#484848",
+    fontWeight: "bold",
     fontSize: 18,
-    lineHeight: 21,
+    lineHeight: 21
   },
   inputTakerContainer: {
     flex: 0.6,
-    alignItems: 'flex-start',
-  },
+    alignItems: "flex-start"
+  }
 });
 
 export default CreateGoal;
