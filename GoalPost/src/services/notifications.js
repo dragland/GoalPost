d45bacbc294dfcb40246e4cbe7f41b5da1963a2e
dragland <dragland@stanford.edu>
2019-06-04 19:56:03 -0700

    saddo mode.

--- GoalPost/src/services/notifications.js
+++ GoalPost/src/services/notifications.js
@@ -5,15 +5,8 @@ class localNotificationManager {
   constructor() {
     this.PushNotification = require('react-native-push-notification');
     this.PushNotification.configure({
-        onNotification: function(info) {
-          if (info.action === "Yes") {
-            Cloud.checkInToEvent(info.userID, info.goalID, info.eventIndex, true);
-            this.cancelLocalNotifications({id: info.notificationId});
-          }
-          if (info.action === "No") {
-            Cloud.checkInToEvent(info.userID, info.goalID, info.eventIndex, false);
-            this.cancelLocalNotifications({id: info.notificationId});
-          }
+        onNotification: function(notification) {
+          console.log(notification);
         },
         popInitialNotification: true,
         requestPermissions: true,
@@ -25,8 +18,8 @@ class localNotificationManager {
     let goal = await Cloud.loadGoal(userID, goalID);
     let eventTimes = goal.event_times;
     let goalName = goal.goal_name;
-    for (let ind = 0; ind < eventTimes.length; ind++) {
-      let currDate = eventTimes[ind].toDate();
+    for (let i = 0; i < eventTimes.length; i++) {
+      let currDate = eventTimes[i].toDate();
       this.PushNotification.localNotificationSchedule({
         largeIcon: "ic_launcher",
         smallIcon: "ic_notification",
@@ -34,10 +27,9 @@ class localNotificationManager {
         title: "Hi " + userName.split(" ")[0] + ", make sure to check in!",
         message: "Did you make it to your event today for " + goalName + "?",
         playSound: false,
-        actions: '["Yes", "No"]',
         userID: userID,
         goalID: goalID,
-        eventIndex: ind,
+        eventIndex: i,
         date: currDate
       });
     }
