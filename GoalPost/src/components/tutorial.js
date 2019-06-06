a2a72cf89f021843ffc03fa119cb2d9e9cd37014
dragland <dragland@stanford.edu>
2019-06-05 22:09:40 -0700

    wrote user guide for login tutorial.

---
+++ GoalPost/src/components/tutorial.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import baseStyles from "../../styles/baseStyles";
+
+export default class Tutorial extends Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text style={styles.title}>
+          <Text style={{ fontWeight: 'bold' }}>
+            GoalPost is a social app that incentivizes users to help reach their goals through collaborative dynamics and friendly competition :)
+          </Text>
+        </Text>
+        <Text style={styles.break}>{"\n"}</Text>
+        <Text style={styles.block}><Text style={{ fontWeight: 'bold' }}>Goals</Text></Text>
+        <Text style={styles.block}>
+          Is there something you always wanted to achieve that felt far away? Take your first baby steps by creating a goal!
+          {"\n\n"}
+          By clicking on the big plus button at the top of the home page, you will be able to create a goal that you want to accomplish.
+          {"\n\n"}
+          You can select when you want it to start, when you want it to end, and each day that you want to take a baby step "event" on.
+          {"\n\n"}
+          Also, as a personal incentive, you can set a penalty for each time an event is missed.
+        </Text>
+        <Text style={styles.break}>{"\n"}</Text>
+        <Text style={styles.block}><Text style={{ fontWeight: 'bold' }}>Friends</Text></Text>
+        <Text style={styles.block}>
+          Goals are better with friends! When you create a new goal, you can invite friends on the app to help you complete it.
+          {"\n\n"}
+          Also, on your home screen, you can see pending goal invitations from your friends. You can then click on it to view the details and decide if you want to join.
+        </Text>
+        <Text style={styles.break}>{"\n"}</Text>
+        <Text style={styles.block}><Text style={{ fontWeight: 'bold' }}>Checking in</Text></Text>
+        <Text style={styles.block}>
+          Once your goal is active, you will be reminded each event day to check in. You can do so by navigating to that goal's page from your home list.
+          {"\n\n"}
+          If you have completed your event for the day, check in and your overall progress score will go up!
+          If you fail however, you will be charged the set penalty.
+        </Text>
+        <Text style={styles.break}>{"\n"}</Text>
+        <Text style={styles.block}><Text style={{ fontWeight: 'bold' }}>Money</Text></Text>
+        <Text style={styles.block}>
+          Staying on track with your goals can be hard. Luckily, we have a fun and playful incentive for you: your friend's money!
+          {"\n\n"}
+          Each time you or your friends miss an event, the money gets added to the pot, and at the end, the pot gets split evenly between all goal members.
+          {"\n\n"}
+          If you stay on track and make it to the end of the goal, you will earn money, but if you miss more events than your friends, their dinner will be on you.
+          {"\n\n"}
+          Have fun and good luck on all your goals!
+        </Text>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: "#484848",
+    fontSize: 18,
+    textAlign: "center"
+  },
+  block: {
+    color: "#484848",
+    fontSize: 18,
+    textAlign: "left"
+  },
+  break: {
+    fontSize: 5,
+  }
+});
--- GoalPost/src/screens/login.js
+++ GoalPost/src/screens/login.js
@@ -7,6 +7,7 @@ import Spinner from 'react-native-loading-spinner-overlay';
 import baseStyles from "../../styles/baseStyles";
 import StandardButton from "../components/standardButton";
 import CenterImage from "../components/centerImage";
+import Tutorial from "../components/tutorial";
 
 import { Cloud } from "../services/database";
 import { facebookService } from "../services/FacebookService";
@@ -87,20 +88,14 @@ export default class Login extends React.Component {
               onPress={this.toggleModal}
               color
             />
-            <Modal
+            <Modal 
+              propagateSwipe
               isVisible={this.state.isModalVisible}
               onBackButtonPress={this.toggleModal}
               onBackdropPress={this.toggleModal}
             >
               <View style={styles.modalView}>
-                <Text style={baseStyles.text}>
-                  <Text style={{ fontWeight: 'bold' }}>
-                    GoalPost is a social app that incentivizes users to help reach their goals through collaborative dynamics and friendly competition.
-                  </Text>
-                </Text>
-                <Text style={baseStyles.text}>
-                  For example, if a group of friends wanted to go to the gym every week until summer and hold each other accountable, they would be able to configure this goal through our app and also invite their friends. Then, at each event during the goal, users would check in using the app, facilitating group accountability. As an extra incentive, when a user fails to check in, they will get charged a penalty which goes into a central pot. At the end of the goal, the pot gets split evenly, incentivizing users to maximize their goal completion through fostering a culture of collaborative competition.
-                </Text>
+                <Tutorial />
                 <Button title="Got it" onPress={this.toggleModal} />
               </View>
             </Modal>
